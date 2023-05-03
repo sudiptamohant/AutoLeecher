6af3e861c38a8152bdacbeb5a4458e0e88f33f88
sudiptamohant <78973261+sudiptamohant@users.noreply.github.com>
2023-05-04 01:40:19 +0530

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,7 @@
+from apdbot.sample_con import Config
+
+class Config(Config):
+  BOT_TOKEN = "6118812161:AAHCZrTJnGNpIfxGOmzkjGhEP1c5231kYJ0"
+  APP_ID = 5806640
+  API_HASH = "127f130ad3745dbcd31aa39aa0eabcb8"
+  AUTH = [-1001981767301]
